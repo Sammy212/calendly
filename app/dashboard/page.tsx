0e3b84e5d87d211fd77539305d98d00859afdbb9
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-10 05:09:06 +0100

    Added eventtypes route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,47 @@
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../components/EmptyState";
+
+async function getData(userId: string) {
+    const data = await prisma.user.findUnique({
+        where:  {
+            id: userId,
+        },
+        select: {
+            userName: true,
+            eventType: {
+                select: {
+                    id: true,
+                    active: true,
+                    url: true,
+                    duration: true,
+                },
+            },
+        },
+    });
+
+    if(!data) {
+        return notFound();
+    }
+
+    return data;
+}
 
 export default async function DashboardPage() {
 
     const session = await requireUser();
+    const data = await getData(session.user?.id as string);
 
     return (
-        <div className="fle">
-            Dashboard page
-        </div>
+        <>
+            {
+                data.eventType.length === 0 ? (
+                    <EmptyState title="You have no events" description="Create your first event below" buttonText="Add new event" href="/dashboard/new"/>
+                ) : (
+                    <p>See all these events</p>
+                )
+            }
+        </>
     )
 };
